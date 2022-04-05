042c3fb766305482db2d69a7b4f0384210a7ab6c
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-04-05 21:34:26 +0300

    refactoringToHooks

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,18 +1,20 @@
 import PropTypes from 'prop-types';
-import s from './FeedbackOptions.module.css'
+import s from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ onLeaveFeedback, option}) => {
-    return (
-        <button
-            className={s.Feedback__button}
-            onClick={() => onLeaveFeedback(option)}>{option}
-        </button>
-    );
-}
+const FeedbackOptions = ({ onLeaveFeedback, option }) => {
+  return (
+    <button
+      className={s.Feedback__button}
+      onClick={() => onLeaveFeedback(option)}
+    >
+      {option}
+    </button>
+  );
+};
 
 FeedbackOptions.propTypes = {
-    onLeaveFeedback: PropTypes.func.isRequired,
-    option: PropTypes.string.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+  option: PropTypes.string.isRequired,
 };
 
 export default FeedbackOptions;
